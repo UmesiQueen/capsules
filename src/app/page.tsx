725108030e20a422f6b0fe9c2cdcb19ca6c9bdcc
Queen Umesi <queenumesi01@gmail.com>
2025-12-10 17:28:03 +0100

    feat: implement capsule card animations and enhance scroll interactions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,12 @@ import Marquee from "react-fast-marquee";
 import Capsule from "@/components/CapsuleCard";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import TextRevealAnimation from "@/components/TextRevealAnimation";
 
 export default function Home() {
-  const containerRef = React.useRef<HTMLDivElement>(null);
-  const chooseTextRef = React.useRef<HTMLDivElement>(null);
-  const bestTextRef = React.useRef<HTMLDivElement>(null);
-
   useGSAP(() => {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
     ScrollSmoother.create({
       wrapper: "#smooth-wrapper",
@@ -22,14 +18,12 @@ export default function Home() {
     });
 
     // hero section background zoom effect
-    if (!containerRef.current) return;
-    const container = containerRef.current;
-
-    gsap.to(container, {
+    const hero = document.querySelector(".hero") as HTMLDivElement;
+    gsap.to(hero, {
       scale: 1.1,
       paused: true,
       scrollTrigger: {
-        trigger: container,
+        trigger: hero,
         start: "top top", // Start when top of element hits top of viewport
         end: "bottom 25%", // End when bottom of element hits 25% of viewport
         scrub: true,
@@ -37,20 +31,210 @@ export default function Home() {
     });
 
     // choose the one text animation
-    if (!chooseTextRef.current && !bestTextRef.current) return;
-    const chooseText = chooseTextRef.current;
-    const bestText = bestTextRef.current;
-
-    gsap.to([chooseText, bestText], {
+    const chooseTextElements = document.querySelectorAll(".chooseText");
+    gsap.to(chooseTextElements, {
       clipPath: "inset(0% 0 -20px 0)",
       y: "0",
       scrollTrigger: {
-        trigger: [chooseText, bestText],
+        trigger: chooseTextElements,
         start: "bottom 80%",
         end: "bottom 20%",
         scrub: true,
       },
     });
+
+    // capsule cards content animation
+    const contentRevealAnimation = (card: string) => {
+      const capsuleDialogBtn = document.querySelector(
+        `.capsule-container .${card} .dialog-btn`
+      );
+      const capsuleCardDescription = document.querySelector(
+        `.capsule-container .${card} .card-description`
+      );
+      const capsuleCardTitle = document.querySelector(
+        `.capsule-container .${card} .card-title`
+      );
+      const splitCardTitle = SplitText.create(capsuleCardTitle, {
+        type: "chars",
+        mask: "chars",
+      });
+
+      const textRevealTl = gsap
+        .timeline({ paused: true })
+        .add("fadeIn")
+        .from(splitCardTitle.chars, { x: "100%", autoAlpha: 0 }, "fadeIn")
+        .fromTo(
+          capsuleCardDescription,
+          { x: "20%", opacity: 0 },
+          { x: "0%", opacity: 1, ease: "power4.out" },
+          "fadeIn"
+        )
+        .fromTo(
+          capsuleDialogBtn,
+          { opacity: 0, height: 0, width: 0 },
+          {
+            opacity: 1,
+            height: "40px",
+            width: "40px",
+            ease: "power4.out",
+          },
+          "fadeIn"
+        );
+      return textRevealTl;
+    };
+    // create instances for each card
+    const card1RevealTl = contentRevealAnimation("card1");
+    const card2RevealTl = contentRevealAnimation("card2");
+    const card3RevealTl = contentRevealAnimation("card3");
+
+    const scrollIndicator = gsap
+      .timeline({ paused: true })
+      .fromTo(
+        ".scroll-indicator",
+        { autoAlpha: 0, x: 20 },
+        { autoAlpha: 1, x: 0 }
+      );
+
+    const capsuleContainer = document.querySelector(".capsule-container");
+    const capsuleCardsTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: capsuleContainer,
+        start: "top top",
+        end: "+=500%",
+        pin: true,
+        scrub: 1,
+        onUpdate: (timeline) => {
+          gsap.to(".capsule-progress", {
+            width: `${timeline.progress * 100}%`,
+          });
+        },
+      },
+    });
+
+    capsuleCardsTl
+      .add("card1OnEnter")
+      .fromTo(
+        ".capsule-container .card1 .capsule-card",
+        {
+          height: "50%",
+          width: "50%",
+          borderRadius: "100px",
+        },
+        {
+          height: "calc(100% - 16px)",
+          width: "100%",
+          borderRadius: "50px",
+        },
+        "card1OnEnter"
+      )
+      .fromTo(
+        ".capsule-container .card1 .capsule-marquee",
+        { opacity: 1 },
+        { opacity: 0 },
+        "card1OnEnter"
+      )
+      .fromTo(
+        ".capsule-container .card1 .card-img",
+        { scale: 1.3 },
+        { scale: 1 },
+        "card1OnEnter"
+      )
+      .fromTo(
+        ".capsule-container .card1 .card-smoke",
+        { opacity: 0 },
+        { opacity: 0.5 },
+        "card1OnEnter"
+      )
+      .add("contentReveal")
+      .to(
+        {},
+        {
+          duration: 0.15,
+          onStart: () => {
+            card1RevealTl.play();
+            scrollIndicator.play();
+          },
+          onReverseComplete: () => {
+            card1RevealTl.reverse();
+            scrollIndicator.reverse();
+          },
+        },
+        "contentReveal"
+      )
+      .add("card1OnLeave")
+      .to(
+        ".capsule-container .card1 .capsule-card",
+        { scale: 0.9 },
+        "card1OnLeave"
+      )
+      .fromTo(
+        ".capsule-container .card1 .overlay",
+        { autoAlpha: 0 },
+        { autoAlpha: 1 },
+        "card1OnLeave"
+      )
+      .add("card2OnEnter", "<")
+      .fromTo(
+        ".capsule-container .card2",
+        { y: "100dvh" },
+        { y: "0dvh" },
+        "card2OnEnter"
+      )
+      .fromTo(
+        ".capsule-container .card2 .card-img",
+        { scale: 1.3 },
+        { scale: 1 },
+        "card2OnEnter"
+      )
+      .add("card2ContentReveal")
+      .to(
+        {},
+        {
+          duration: 0.15,
+          onStart: () => {
+            card2RevealTl.play();
+          },
+          onReverseComplete: () => {
+            card2RevealTl.reverse();
+          },
+        },
+        "card2ContentReveal"
+      )
+      .add("card2OnLeave")
+      .to(".capsule-container .card2", { scale: 0.9 }, "card2OnLeave")
+      .fromTo(
+        ".capsule-container .card2 .overlay",
+        { autoAlpha: 0 },
+        { autoAlpha: 1 },
+        "card2OnLeave"
+      )
+      .add("card3OnEnter", "<")
+      .fromTo(
+        ".capsule-container .card3",
+        { y: "100dvh" },
+        { y: "0dvh" },
+        "card3OnEnter"
+      )
+      .fromTo(
+        ".capsule-container .card3 .card-img",
+        { scale: 1.3 },
+        { scale: 1 },
+        "card3OnEnter"
+      )
+      .add("card3ContentReveal")
+      .to(
+        {},
+        {
+          duration: 0.15,
+          onStart: () => {
+            card3RevealTl.play();
+          },
+          onReverseComplete: () => {
+            card3RevealTl.reverse();
+          },
+        },
+        "card3ContentReveal"
+      );
   });
 
   return (
@@ -59,10 +243,7 @@ export default function Home() {
         <div className="p-2 font-sans">
           <section className="mb-20">
             <div className="min-h-[calc(100vh-16px)] rounded-[50px] relative overflow-hidden">
-              <div
-                ref={containerRef}
-                className="h-full w-full bg-[url('/img/cap1.png')] scale-[1] bg-no-repeat bg-center bg-cover absolute top-0 left-0 saturate-[120%]"
-              />
+              <div className="hero h-full w-full bg-[url('/img/cap1.png')] scale-[1] bg-no-repeat bg-center bg-cover absolute top-0 left-0 saturate-[120%]" />
               <video
                 width="320"
                 height="240"
@@ -77,6 +258,7 @@ export default function Home() {
               </video>
               <div className="h-full w-full absolute top-0 left-0">
                 <div className="h-full w-full flex flex-col justify-between gap-8 px-6 py-7">
+                  {/* change this to h1 with splitText */}
                   <Image
                     src={"/img/capsule.svg"}
                     alt="Capsules Logo"
@@ -142,16 +324,10 @@ export default function Home() {
               <p className="text-sm pb-5">Discover available Capsules®</p>
             </div>
             <div className="text-[184px] text-nowrap leading-none">
-              <div
-                ref={chooseTextRef}
-                className="[clip-path:inset(100%_0_-20px_0)] -translate-y-[50px] "
-              >
+              <div className="chooseText [clip-path:inset(100%_0_-20px_0)] -translate-y-[50px]">
                 <div>Choose the one</div>
               </div>
-              <div
-                ref={bestTextRef}
-                className="[clip-path:inset(100%_0_-20px_0)] -translate-y-[50px] "
-              >
+              <div className="chooseText [clip-path:inset(100%_0_-20px_0)] -translate-y-[50px] ">
                 <div>you like best</div>
               </div>
             </div>
@@ -180,37 +356,41 @@ export default function Home() {
               </div>
             </div>
           </section>
-          <section className=" min-h-[calc(100vh-16px)] overflow-hidden">
-            <div className="w-full h-[calc(100vh-16px)] flex items-center">
-              <Marquee autoFill={true} speed={150}>
-                <Image
-                  src={"/img/capsule.svg"}
-                  alt="Capsules Logo"
-                  width={500}
-                  height={500}
-                  className="h-48 w-fit px-5"
+          <section className="capsule-container relative h-dvh">
+            <div className="card1 absolute left-0 top-0 h-full w-full z-0">
+              <div className="h-dvh w-full relative overflow-hidden flex items-center justify-center">
+                <div className="capsule-marquee w-full flex items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0">
+                  <Marquee autoFill={true} speed={150}>
+                    <Image
+                      src={"/img/capsule.svg"}
+                      alt="Capsules Logo"
+                      width={500}
+                      height={500}
+                      className="h-48 w-fit px-5"
+                    />
+                  </Marquee>
+                </div>
+                {/* Classic */}
+                <Capsule
+                  title={"Classic"}
+                  description={
+                    "Classic Capsule® boasts refined aesthetics and a modern interior, creating an intimate retreat in a desert landscape."
+                  }
+                  features={[
+                    "22m2",
+                    "King Size",
+                    "Available",
+                    "Available",
+                    "Available",
+                    "None",
+                  ]}
+                  imgSrc={"/img/cap3.png"}
+                  cost={2000}
                 />
-              </Marquee>
+              </div>
             </div>
-            <div className="space-y-2">
-              {/* Classic */}
-              <Capsule
-                title={"Classic"}
-                description={
-                  "Classic Capsule® boasts refined aesthetics and a modern interior, creating an intimate retreat in a desert landscape."
-                }
-                features={[
-                  "22m2",
-                  "King Size",
-                  "Available",
-                  "Available",
-                  "Available",
-                  "None",
-                ]}
-                imgSrc={"/img/cap3.png"}
-                cost={2000}
-              />
-              {/* Terrance */}
+            {/* Terrance */}
+            <div className="card2 absolute left-0 top-0 h-full w-full z-1 translate-y-[100dvh] py-2">
               <Capsule
                 title={"Terrance"}
                 description={
@@ -227,7 +407,9 @@ export default function Home() {
                 imgSrc={"/img/cap2.png"}
                 cost={2500}
               />
-              {/* Desert */}
+            </div>
+            {/* Desert */}
+            <div className=" card3 absolute left-0 top-0 h-full w-full z-2 translate-y-[100dvh] py-2">
               <Capsule
                 title={"Desert"}
                 description={
@@ -244,6 +426,12 @@ export default function Home() {
                 imgSrc={"/img/cap1.png"}
                 cost={2250}
               />
+            </div>
+            <div className="scroll-indicator absolute top-1/2 bottom-1/2 -translate-y-1/2 right-5 z-10">
+              <p className="text-[40px] text-white/50">(Scroll)</p>
+            </div>
+            <div className="scroll-indicator absolute bottom-15 right-5 w-80 h-[2px] rounded-md bg-white/30 z-10 inline-flex overflow-hidden">
+              <div className="capsule-progress w-0 h-full bg-white" />
             </div>
           </section>
         </div>
